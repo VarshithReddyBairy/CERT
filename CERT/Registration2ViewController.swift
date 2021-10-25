48cd14d3780e591ab5124a14bbfdbd942e7cbd3f
JayaShankar M <jaysh309@gmail.com>
2021-10-24 19:01:33 -0500

    Registration2ViewController

---
+++ CERT/Registration2ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Registration2ViewController.swift
+//  CERT
+//
+//  Created by JayaShankar Mangina on 10/24/21.
+//
+
+import UIKit
+
+class Registration2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
