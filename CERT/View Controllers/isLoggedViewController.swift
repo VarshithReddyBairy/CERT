5e75c13becb47a4859ed4744c854ae7776937125
JayaShankar M <jaysh309@gmail.com>
2021-11-12 00:51:21 -0600

    Update isLoggedViewController.swift
    Changed the isLoggedVC to waitListVC

--- CERT/View Controllers/isLoggedViewController.swift
+++ CERT/View Controllers/isLoggedViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class isLoggedViewController: UIViewController {
 
+    @IBOutlet weak var messageView: UITextView!
+    @IBOutlet weak var subjectLabel: UILabel!
     @IBOutlet weak var displayLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        displayLabel.text = "Welcome Onboard!."
+        displayLabel.text = "Welcome!"
+        subjectLabel.text = "You're on the Waitlist."
+        messageView.text = "You'll be notified, when your request got Approved. Thanks for your patience. You may now exit the application"
     }
     
 
