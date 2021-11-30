16a6ba967415071d2a4b98c85f39fea4a6c75e1c
JayaShankar M <jaysh309@gmail.com>
2021-11-30 17:13:00 -0600

    Create launchScreenViewController.swift

---
+++ CERT/View Controllers/launchScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  launchScreenViewController.swift
+//  CERT
+//
+//  Created by JayaShankar Mangina on 11/30/21.
+//
+
+import UIKit
+
+class launchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
