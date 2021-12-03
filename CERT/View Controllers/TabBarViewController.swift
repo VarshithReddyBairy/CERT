1a431f93fd5a4b2598c2cbacdc12d97164e91a8c
JayaShankar M <jaysh309@gmail.com>
2021-12-02 19:01:24 -0600

    Create TabBarViewController.swift

---
+++ CERT/View Controllers/TabBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarViewController.swift
+//  CERT
+//
+//  Created by JayaShankar Mangina on 12/2/21.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
