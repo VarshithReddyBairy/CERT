b3d630ae3f3cdde9ef3992b6ff2cbb5ce7f8e003
JayaShankar Mangina <79549340+jyshnkr@users.noreply.github.com>
2022-02-20 13:20:46 -0600

    Create roundedImageView.swift

---
+++ CERT/Helper Files/roundedImageView.swift
@@ -0,0 +1,25 @@
+//
+//  roundedImageView.swift
+//  CERT
+//
+//  Created by JayaShankar Mangina on 2/20/22.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedImageView: UIImageView {
+    @IBInspectable var roundedImageView:Bool = false {
+        didSet {
+            if roundedImageView {
+                layer.cornerRadius = frame.height/2
+            }
+        }
+    }
+    override func prepareForInterfaceBuilder() {
+        if roundedImageView {
+            layer.cornerRadius = frame.height/2
+        }
+    }
+
+}
