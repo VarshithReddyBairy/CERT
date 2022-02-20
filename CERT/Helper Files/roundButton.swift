3b97563a24519d1e42c78d666a7e5d927dfd013d
JayaShankar Mangina <79549340+jyshnkr@users.noreply.github.com>
2022-02-20 13:20:48 -0600

    Update roundButton.swift

--- CERT/Helper Files/roundButton.swift
+++ CERT/Helper Files/roundButton.swift
@@ -12,14 +12,15 @@ class RoundButton: UIButton {
     @IBInspectable var roundButton:Bool = false {
         didSet {
             if roundButton {
-                layer.cornerRadius = frame.height / 2
+                layer.cornerRadius = frame.height/2
             }
         }
     }
     override func prepareForInterfaceBuilder() {
         if roundButton {
-            layer.cornerRadius = frame.height / 2
+            layer.cornerRadius = frame.height/2
         }
     }
 
 }
+
