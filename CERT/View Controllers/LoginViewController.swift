2b9c84735331ba89b6cd290b39814e0605aad1a0
JayaShankar Mangina <79549340+jyshnkr@users.noreply.github.com>
2022-03-27 15:32:50 -0500

    Update LoginViewController.swift

--- CERT/View Controllers/LoginViewController.swift
+++ CERT/View Controllers/LoginViewController.swift
@@ -50,6 +50,7 @@ class LoginViewController: UIViewController {
                 }
             }
         }
+        performSegue(withIdentifier: "volunteerHome", sender: UIButton.self)
     }
     
 }
