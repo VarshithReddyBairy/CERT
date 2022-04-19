03b243f5e22689722fcbd45904f4fd35417aae50
VarshithReddyBairy <varshith.bairy10@gmail.com>
2022-04-18 21:28:25 -0500

    Update VolunteerHomeViewController.swift

--- CERT/View Controllers/VolunteerHomeViewController.swift
+++ CERT/View Controllers/VolunteerHomeViewController.swift
@@ -2,34 +2,40 @@
 //  VolunteerHomeViewController.swift
 //  CERT
 //
-//  Created by JayaShankar Mangina on 2/21/22.
+//  Created by CERT on 2/21/22.
 //
 
 import UIKit
 import FirebaseAuth
 import Firebase
 import FirebaseDatabase
-class UserDetails{
-    
-}
+
+
 
 class VolunteerHomeViewController: UIViewController {
 
+    var lastName: String = ""
+    var streetAddress: String = ""
+    var firstName: String = ""
+    var emailAddress: String = ""
+    var qualification: String = ""
+    var state: String = ""
+    var verifyPassword: String = ""
+    var createdAt: String = ""
+    var city: String = ""
+    var others: String = ""
+    var contactNumber: String = ""
     var userName : String?
     @IBOutlet weak var greetingLabel: UILabel!
     @IBOutlet weak var reportButtoOutlet: UIButton!
     @IBOutlet weak var volunteerImageView: UIImageView!
     
-    var firstName : String?
-    var lastName : String?
-    var contactNumber : Int?
-    var emailAdd : String?
-    var address : String?
-    var city : String?
-    var state : String?
-    var qualification : String?
-    var details = ["First Name: Vinay", "Last Name : Paspula", "Contact Number : 6602158877" , "EmailId : vinaykmr1490@gmail.com" , "Address : 1115 N College Dr", "City : Maryville", "State : Missouri", "Qualification : CERT Trainee"]
+   
+//    var details = ["First Name: Vinay", "Last Name : Paspula", "Contact Number : 6602158877" , "EmailId : vinaykmr1490@gmail.com" , "Address : 1115 N College Dr", "City : Maryville", "State : Missouri", "Qualification : CERT Trainee"]
+//
     
+    var details :  [ String] = []
+    var detailsUpdated : [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = ""
@@ -43,12 +49,93 @@ class VolunteerHomeViewController: UIViewController {
 //                    self.firstName = dictionary["firstName"] as? String
 //                }
 //            }, withCancel: nil)
-            Database.database().reference().child("usersDB").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
-                if let dictionary = snapshot.value as? [String: AnyObject]{
-                    self.firstName = dictionary["firstName"] as? String
+//            Database.database().reference().child("usersDB").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
+//                if let dictionary = snapshot.value as? [String: AnyObject]{
+//                    self.firstName = dictionary["firstName"] as? String
+//                }
+//
+//            }, withCancel: nil)
+//
+//            Database.database().reference().child("reportsDB").observeSingleEvent(of: .value, with: { (snapshot) in
+//                print("Reports are")
+//                    print(snapshot)
+//                }
+//
+//        },withCancel: nil)
+            let db = Firestore.firestore()
+//            db.collection("reportsDB").getDocuments{(snapshot ,error) in
+//                if error == nil {
+//                    for document in snapshot!.documents{
+//                        let docdata = document.data()
+//                        print("reports data")
+//                        print(docdata)
+//                    }
+//                }
+//            }
+            
+            db.collection("usersDB").whereField("emailAddress", isEqualTo: userName).getDocuments{(snapshot ,error) in
+                if error == nil {
+
+                    for document in snapshot!.documents{
+                        print(type(of: document))
+                        let docdata = document.data()
+                        print(type(of: docdata))
+                        print("users data")
+                        print(docdata)
+                        
+                        print("---------------")
+                        for (key, value) in docdata{
+                            print("key is \(key) : value is \(value)")
+                            var newvalue = value as! String
+                            self.details.append(newvalue)
+                            if key == "firstName"{
+                                self.firstName = "First Name : \(newvalue)"
+                                print("printing first name : \(self.firstName)")
+                            }
+                            if key == "lastName"{
+                                self.lastName = "last Name : \(newvalue)"
+                            }
+                            if key == "emailAddress"{
+                                self.emailAddress = "Email Address : \(newvalue)"
+                            }
+                           
+                            
+                            if key == "streetAddress"{
+                                self.streetAddress = "Street Address : \(newvalue)"
+                            }
+                            if key == "city"{
+                                self.city = "City : \(newvalue)"
+                            }
+                            if key == "state"{
+                                self.state = "State : \(newvalue)"
+                            }
+                            if key == "contactNumber"{
+                                self.contactNumber = "Contact Number : \(newvalue)"
+                            }
+                            
+                            if key == "qualification"{
+                                self.qualification = "Qualification : \(newvalue)"
+                            }
+                        }
+                    }
                 }
-                
-            }, withCancel: nil)
+            }
+            
+            
+           
+            
+            
+//            db.collection("cities").whereField("capital", isEqualTo: true)
+//                .getDocuments() { (querySnapshot, err) in
+//                    if let err = err {
+//                        print("Error getting documents: \(err)")
+//                    } else {
+//                        for document in querySnapshot!.documents {
+//                            print("\(document.documentID) => \(document.data())")
+//                        }
+//                    }
+//            }
+
         } else {
           print("Authentication Error")
         }
@@ -57,7 +144,12 @@ class VolunteerHomeViewController: UIViewController {
         //print("firstName \(firstName)")
         //greetingLabel.text = "Welcome, \(firstName)"
         //
+        
+        
         reportButtoOutlet.createFloatingActionButton()
+        
+        
+      
     }
     
 
@@ -83,7 +175,17 @@ class VolunteerHomeViewController: UIViewController {
         
         if identifier == "profileDetails" {
             let destination = segue.destination as! ProfileViewController
-            destination.profile = details
+            print("printing the values of new array")
+            print(detailsUpdated)
+            detailsUpdated.append(firstName)
+            detailsUpdated.append(lastName)
+            detailsUpdated.append(emailAddress)
+            detailsUpdated.append(streetAddress)
+            detailsUpdated.append(city)
+            detailsUpdated.append(state)
+            detailsUpdated.append(contactNumber)
+            detailsUpdated.append(qualification)
+            destination.profile = detailsUpdated
         }
     }
     
